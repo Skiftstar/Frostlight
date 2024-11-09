dc993a34b253335805eb9d5ab1030510f7317764
Skiftstar <yukamitft@gmail.com>
2024-11-10 00:35:33 +0100

    init

--- src/Windows/AudioWindow/MediaControl/components/Buttons/NextButton.js
+++
@@ -1,9 +0,0 @@
-const NextButton = (player) =>
-	Widget.Button({
-		class_name: "next-button",
-		onClicked: () => player.next(),
-		child: Widget.Icon("media-skip-forward-symbolic"),
-		visible: player.bind("can-go-next"),
-	});
-
-export default NextButton;
